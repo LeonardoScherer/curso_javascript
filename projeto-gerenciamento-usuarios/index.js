f8aa5d614162513de6f205e8faba90a6008111c4
Leonardo Scherer <leoblacena11@gmail.com>
2022-05-15 13:35:40 -0300

    Manipulando DOM, variáveis e seleores e forEach

---
+++ projeto-gerenciamento-usuarios/index.js
@@ -0,0 +1,16 @@
+var name = document.querySelector('#exampleInputName');
+var gender = document.querySelector('#form-user-create [name=gender]:checked');
+var birth = document.querySelector('#exampleInputBirth');
+var contry = document.querySelector('#exampleInputContry');
+var email = document.querySelector('#exampleInputEmail');
+var password = document.querySelector('#exampleInputPassword');
+var photo = document.querySelector('#exampleInputPhoto');
+var admin = document.querySelector('#exampleInputAdmin');
+
+var fields = document.querySelectorAll('#form-user-create [name]');
+
+fields.forEach(function(field, index) {
+
+    console.log(index);
+
+});
